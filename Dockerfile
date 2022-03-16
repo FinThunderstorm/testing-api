5e91aa8e4172d13cde102fa76dc3b77f01833c48
Tuomas Alanen <tualanen@cs.helsinki.fi>
2022-03-16 10:41:46 +0200

    fixed mistakes, added second type

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
 FROM node:17-buster
 
-RUN mkdir /api && chown node:node /api
-USER node
-
 WORKDIR /api
-
 
 COPY package.json package-lock.json ./
 
-
-RUN npm ci --include=dev
+RUN npm ci
 
 EXPOSE 3000
 
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const fileUpoad = require('express-fileupload')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const { Random } = require('random-js')
 
 const app = express()
 const port = 3000
 
+const random = new Random()
 
 app.use(fileUpoad({
   createParentPath: true
@@ -27,25 +29,39 @@ app.post('/', (req, res) => {
                 return
             } else {
                 const audioFile = req.files.file
-                res.json({
-                "Prompt": req.query.prompt,
-                "Language": req.query.lang,
-                "Task": req.query.task,
-                "file_name": audioFile.name,
-                "Transcript": "Lorem ipsum dolor sit amet.",
-                "Fluency": {
-                    "score": 3.00,
-                    "mean_f1": 0.19,
-                    "speech_rate": 3.12,
-                },
-                "TaskAchievement": 0.40,
-                "Accuracy": {
-                    "score": 2.00,
-                    "lexical_profile": 3.12,
-                    "nativeity": 0.40,
-                },
-                "Holistic": 2.40,
-                })
+                const freeform = {
+                    "prompt": req.query.prompt,
+                    "Language": req.query.lang,
+                    "Task": req.query.task,
+                    "file_name": audioFile.name,
+                    "Transcript": "Lorem ipsum dolor sit amet.",
+                    "Fluency": {
+                        "score": 3.00,
+                        "mean_f1": 0.19,
+                        "speech_rate": 3.12,
+                    },
+                    "TaskAchievement": 0.40,
+                    "Accuracy": {
+                        "score": 2.00,
+                        "lexical_profile": 3.12,
+                        "nativeity": 0.40,
+                    },
+                    "Holistic": 2.40,
+                }
+                const readaloud = {
+                    "prompt": req.query.prompt,
+                    "Language": req.query.lang,
+                    "Task": req.query.task,
+                    "file_name": audioFile.name,
+                    "GOP_score": 0.7,
+                }
+
+                const answer = req.query.task === "freeform" ? freeform : readaloud
+
+                const waitTime = random.integer(0,10)*1000
+                setTimeout(() => {
+                    res.json(answer)
+                }, waitTime)
             }
         } else {
             res.status(418).send("418 I'm a teapot - 2")
